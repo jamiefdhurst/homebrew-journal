962853aa42a96f715602679df6b3222f59010a1f
Jamie Hurst <jamie@jamiehurst.co.uk>
2026-04-12 19:39:24 +0100

    v0.9.6: change binaries

--- Formula/journal.rb
+++ Formula/journal.rb
@@ -6,17 +6,18 @@ class Journal < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/jamiefdhurst/journal/releases/download/v#{version}/journal_darwin_arm64-#{version}"
-      sha256 "ba5a0e67faa6e355840d33ea3be30c21e153c55da33376b3e66afb42f022dce2"
+      url "https://github.com/jamiefdhurst/journal/releases/download/v#{version}/journal-#{version}-darwin-arm64.zip"
+      sha256 "65cc8fe37dc0787aface24d2da3b9358d3980e0a8574f9ce957d6b41a6e4268c"
     end
     on_intel do
-      url "https://github.com/jamiefdhurst/journal/releases/download/v#{version}/journal_darwin_amd64-#{version}"
-      sha256 "4c92ef421e74c0c278698e77608cc07417685691e9e115954d2cd3fb0f0e2558"
+      url "https://github.com/jamiefdhurst/journal/releases/download/v#{version}/journal-#{version}-darwin-amd64.zip"
+      sha256 "6232507aa7315938c473bfe8ac88975a3d56b769e2e959106fdb0cd4ae75d364"
     end
   end
 
   def install
-    bin.install Dir["journal_darwin_*"].first => "journal"
+    libexec.install "journal", "web"
+    bin.install_symlink libexec/"journal"
   end
 
   test do
